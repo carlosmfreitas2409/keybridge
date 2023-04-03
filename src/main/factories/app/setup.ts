bc5de393d7a95e0f47339409c41d3fecb073e7ae
Carlos Eduardo Marques Freitas <carlosmfreitas05@gmail.com>
2023-04-02 23:25:21 -0300

    fix: only initializate app in system startup in production

--- src/main/factories/app/setup.ts
+++ src/main/factories/app/setup.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 
-import { PLATFORM } from '@shared/constants';
+import { PLATFORM, ENVIRONMENT } from '@shared/constants';
 
 export function makeAppSetup(createWindow: () => BrowserWindow) {
   let window = createWindow();
@@ -15,9 +15,11 @@ export function makeAppSetup(createWindow: () => BrowserWindow) {
 
   app.commandLine.appendSwitch('enable-transparent-visuals');
 
-  app.setLoginItemSettings({
-    openAtLogin: true,
-  });
+  !ENVIRONMENT.IS_DEV &&
+    app.setLoginItemSettings({
+      openAtLogin: true,
+      openAsHidden: true,
+    });
 
   return window;
 }
